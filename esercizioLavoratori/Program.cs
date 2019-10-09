535df921352721cff5401aa0212b7170d6702803
jacopodarco <jacopo.darco@gmail.com>
2019-10-09 17:10:01 +0200

    ee

--- esercizioLavoratori/Program.cs
+++ esercizioLavoratori/Program.cs
@@ -16,64 +16,66 @@ namespace esercizioLavoratori
             switch (w) {
                 case 1:
 
-            Console.WriteLine("Sei in Lavoratori autonomi \r\n" + 
-                "Indicare un'operazione: \r\n \r\n" + "1 = Lista dei lavoratori"
-             + "\r\n2 = Calcolo stipendio mensile \r\n3 = Calcolo tasse da pagare" +
-             "\r\n4 = Ordina lavoratori in base allo stipendio" +
-             "\r\n5 = Ordina lavoratori in base agli anni di lavoro");
-            int z = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Sei in Lavoratori autonomi \r\n" + 
+                    "Indicare un'operazione: \r\n \r\n" + "1 = Lista dei lavoratori"
+                    + "\r\n2 = Calcolo stipendio mensile \r\n3 = Calcolo tasse da pagare" +
+                    "\r\n4 = Ordina lavoratori in base allo stipendio" +
+                    "\r\n5 = Ordina lavoratori in base agli anni di lavoro");
+                    int z = Int32.Parse(Console.ReadLine());
 
-            LavoratoriAutonomi[] array = new LavoratoriAutonomi[3];
-                    array[0] = new LavoratoriAutonomi("Luca", "Marinello", 34, Sesso.M) { AnniAttivita = 42, BilancioMedio = 100000 };
-                    array[1] = new LavoratoriAutonomi("Luca", "Prandin", 44, Sesso.M) { AnniAttivita = 13, BilancioMedio = 34897 };
-                    array[2] = new LavoratoriAutonomi("Paolo", "Bonolis", 21, Sesso.N) { AnniAttivita = 1, BilancioMedio = 12 };
+                    LavoratoriAutonomi[] array = new LavoratoriAutonomi[3];
+                            array[0] = new LavoratoriAutonomi("Luca", "Marinello", 34, Sesso.M) { AnniAttivita = 42, BilancioMedio = 10 };
+                            array[1] = new LavoratoriAutonomi("Luca", "Prandin", 44, Sesso.M) { AnniAttivita = 13, BilancioMedio = 34897 };
+                            array[2] = new LavoratoriAutonomi("Paolo", "Bonolis", 21, Sesso.N) { AnniAttivita = 19, BilancioMedio = 1442 };
 
                     switch (z)
             {
-                case 1:
-                    foreach (var g in array)
-                    {
-                        Console.WriteLine(g.GetDettaglioLavoratore());
-                    }
-                    Console.ReadLine();
-                    break;
+                    case 1:
+                        foreach (var g in array)
+                        {
+                            Console.WriteLine(g.GetDettaglioLavoratore());
+                        }
+                        Console.ReadLine();
+                        break;
 
-                case 2:           
-                    foreach (var g in array)
-                    {
-                        Console.WriteLine("Il lavoratore Dipendente " + g.Cognome + " guadagna " + g.StipendioMensile());
-                    }
-                    Console.ReadLine();
-                    break;
+                    case 2:           
+                        foreach (var g in array)
+                        {
+                            Console.WriteLine("Il lavoratore Dipendente " + g.Cognome + " guadagna " + g.StipendioMensile());
+                        }
+                        Console.ReadLine();
+                        break;
 
-                case 3:
+                    case 3:
 
-                            foreach (var g in array)
-                            {
-                                Console.WriteLine("Il lavoratore Dipendente " + g.Cognome + " versa " + g.Tasse());
-                            }
-                            Console.ReadLine();
-                            break;
+                        foreach (var g in array)
+                        {
+                            Console.WriteLine("Il lavoratore Dipendente " + g.Cognome + " versa " + g.Tasse());
+                        }
+                        Console.ReadLine();
+                        break;
 
-                case 4:
-                            LavoratoriAutonomi[] sortedAutonomi = array.OrderBy
-                            (lavAutonomi => lavAutonomi.StipendioMensile()).ToArray();
-                            foreach (var g in sortedAutonomi)
-                            {
-                                Console.WriteLine(g.GetDettaglioLavoratore());
-                            }
-                            Console.ReadLine();
+                    case 4:
+                        LavoratoriAutonomi[] sortedAutonomi = array.OrderBy
+                        (lavAutonomi => lavAutonomi.StipendioMensile()).ToArray();
+                        
+                        foreach (var g in sortedAutonomi)
+                        {
+                            Console.WriteLine(g.GetDettaglioLavoratore());
+                        }
+                        Console.ReadLine();
 
-                            break;
-                case 5:
-                            LavoratoriAutonomi[] sortedAutonomi1 = array.OrderBy
-                                   (lavAutonomi => lavAutonomi.AnniAttivita).ToArray();
-                            foreach (var g in sortedAutonomi1)
-                            {
-                                Console.WriteLine(g.GetDettaglioLavoratore());
-                            }
-                            Console.ReadLine();
-                            break;
+                        break;
+                    case 5:
+                        LavoratoriAutonomi[] sortedAutonomi1 = array.OrderBy
+                        (lavAutonomi => lavAutonomi.AnniAttivita).ToArray();
+
+                        foreach (var g in sortedAutonomi1)
+                        {
+                            Console.WriteLine(g.GetDettaglioLavoratore());
+                        }
+                        Console.ReadLine();
+                        break;
             }
                     break;
 
@@ -87,9 +89,9 @@ namespace esercizioLavoratori
                     int x = Int32.Parse(Console.ReadLine());
                     
                     LavoratoriDipendenti[] array1 = new LavoratoriDipendenti[3];
-                    array1[0] = new LavoratoriDipendenti("Sallustio", "Regale", 18, Sesso.M) { Stipendio = 24000, AnniAssunzione = 14 };
-                    array1[1] = new LavoratoriDipendenti("Alberta", "Giorgiani", 78, Sesso.F) { Stipendio = 36000, AnniAssunzione = 24 };
-                    array1[2] = new LavoratoriDipendenti("Marika", "Gonzales", 67, Sesso.F) { Stipendio = 909090, AnniAssunzione = 34 };
+                    array1[0] = new LavoratoriDipendenti("Sallustio", "Regale", 18, Sesso.M) { Stipendio = 240000, AnniAssunzione = 11 };
+                    array1[1] = new LavoratoriDipendenti("Alberta", "Giorgiani", 78, Sesso.F) { Stipendio = 36000, AnniAssunzione = 44 };
+                    array1[2] = new LavoratoriDipendenti("Marika", "Gonzales", 67, Sesso.F) { Stipendio = 909090, AnniAssunzione = 33 };
                     switch (x)
                     {
                         case 1:
@@ -125,6 +127,7 @@ namespace esercizioLavoratori
                         case 4:
                             LavoratoriDipendenti[] sortedAutonomi = array1.OrderBy
                            (lavDipendenti => lavDipendenti.StipendioMensile()).ToArray();
+
                             foreach (var g in sortedAutonomi)
                             {
                                 Console.WriteLine(g.GetDettaglioLavoratore() + g.StipendioMensile());
@@ -134,7 +137,8 @@ namespace esercizioLavoratori
 
                         case 5:
                             LavoratoriDipendenti[] sortedAutonomi1 = array1.OrderBy
-                                (lavAutonomi => lavAutonomi.AnniAssunzione).ToArray();
+                            (lavAutonomi => lavAutonomi.AnniAssunzione).ToArray();
+
                             foreach (var g in sortedAutonomi1)
                             {
                                 Console.WriteLine(g.GetDettaglioLavoratore());
